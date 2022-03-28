647e5e330b345bc68a858966a35f59156fff1835
RutCD <rutchulin0@gmail.com>
2022-03-28 09:14:34 -0500

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,19 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import("../views/About.vue"),
+  },
+  {
+    path: "/detail/:id",
+    name: "detail",
+
+    component: () => import("../views/Detail.vue"),
+  },
+  {
+    path: "/checkout/:id",
+    name: "checkout",
+
+    component: () => import("../views/Checkout.vue"),
   },
 ];
 
